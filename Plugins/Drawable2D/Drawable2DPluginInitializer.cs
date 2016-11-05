a857f76bb1aba9f77631aea057639baa3fdafbbf
informaniac <osakasan@abwesend.de>
2016-11-05 02:13:47 +0100

    Drawable2D Plugin (experimental)

---
+++ Plugins/Drawable2D/Drawable2DPluginInitializer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FIVES;
+
+namespace Drawable2DPlugin
+{
+    public class Drawable2DPluginInitializer : IPluginInitializer
+    {
+        #region IPluginInitializer implementation
+        public string Name
+        {
+            get
+            {
+                return "Drawable2D";
+            }
+        }
+
+        public List<string> ComponentDependencies
+        {
+            get
+            {
+                return new List<String>();
+            }
+        }
+
+        public List<string> PluginDependencies
+        {
+            get
+            {
+                return new List<String>();
+            }
+        }
+
+        public void Initialize()
+        {
+            Console.WriteLine("[Drawable2D] initializing");
+            DefineComponents();
+        }
+
+        public void Shutdown()
+        {
+            Console.WriteLine("[Drawable2D] shutting down");
+        }
+        #endregion
+
+        void DefineComponents()
+        {
+            ComponentDefinition sprite = new ComponentDefinition("sprite");
+            sprite.AddAttribute<String>("textureFile");
+            sprite.AddAttribute<Point>("upperLeftPositionInTextureFile");
+            sprite.AddAttribute<Rectangle>("size");
+            ComponentRegistry.Instance.Register(sprite);
+        }
+    }
+}
