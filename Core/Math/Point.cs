ae0163cbcbe6ac1525944f45b59c24e278b52023
informaniac <osakasan@abwesend.de>
2016-11-05 02:12:36 +0100

    Math Point Class

---
+++ Core/Math/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FIVES
+{
+    [Serializable]
+    public class Point
+    {
+        public int x { get; private set; }
+        public int y { get; private set; }
+
+        public Point() { }
+        public Point(int x, int y) : this()
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return "Point(" + x + "," + y + ")";
+        }
+    }
+}
